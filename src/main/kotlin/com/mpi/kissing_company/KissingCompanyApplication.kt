cea7e6c86bbd42ec16581069cb6c85fec95d0fe1
Machalka <al1999dk@gmail.com>
2022-06-13 00:19:08 +0300

    init

---
+++ src/main/kotlin/com/mpi/kissing_company/KissingCompanyApplication.kt
@@ -0,0 +1,11 @@
+package com.mpi.kissing_company
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KissingCompanyApplication
+
+fun main(args: Array<String>) {
+	runApplication<KissingCompanyApplication>(*args)
+}
---
+++ src/test/kotlin/com/mpi/kissing_company/KissingCompanyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mpi.kissing_company
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KissingCompanyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
