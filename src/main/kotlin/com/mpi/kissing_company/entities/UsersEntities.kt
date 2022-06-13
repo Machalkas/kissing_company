6b8121b37bbbae7b0fbca9ee674213e431ebfd92
Machalka <al1999dk@gmail.com>
2022-06-13 23:34:18 +0300

    create models

--- src/main/kotlin/com/mpi/kissing_company/entities/UsersEntities.kt
+++ src/main/kotlin/com/mpi/kissing_company/entities/UsersEntities.kt
@@ -1,34 +1,34 @@
 package com.mpi.kissing_company.entities
+import java.sql.Timestamp
 import javax.persistence.*
 
 
 @Entity
-@Table(name = "users")
-class User(
+class Users(
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null,
+    @Column(nullable = false)
+    var username: String,
     var first_name: String,
     var second_name: String,
-    var nickname: String,
-    @ManyToOne var role:  Roles
-
-//    fun
+    @Column(nullable = false)
+    var password: String,
+    @ManyToOne(optional = false) var role:  Roles
 )
 
 
 @Entity
-@Table(name = "roles")
 class Roles(
-    @Id @GeneratedValue var id: Long? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Column(nullable = false)
     var name: String
 )
 
 @Entity
-@Table(name = "girls")
 class Girls(
-    @Id @GeneratedValue var id: Long? = null,
-    @ManyToOne var user: User,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @ManyToOne(optional = false) var user: Users,
     var location: String,
     var age: Int,
     var height: Float,
@@ -36,4 +36,74 @@ class Girls(
     var nation: String,
     var telephone: String,
     var hair_color: String
+)
+
+@Entity
+class GirlsPhotos(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Column(unique = true, nullable = false)
+    val photo: String,
+    @ManyToOne(optional = false) val girl: Girls
+
+)
+
+@Entity
+class Feedbacks(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @ManyToOne(optional = false) val user: Users,
+    @ManyToOne(optional = false) val girl: Girls,
+    @Column(columnDefinition = "numeric")
+    var stars: Double,
+    var comment: String
+)
+
+@Entity
+class GirlsFeedbacks(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @ManyToOne(optional = false) val user: Users,
+    @ManyToOne(optional = false) val girl: Girls,
+    @Column(columnDefinition = "numeric")
+    var stars: Double,
+    var comment: String
+)
+
+@Entity
+class PriceList(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @ManyToOne(optional = false) val girl: Girls,
+    @Column(nullable = false)
+    var service_name: String,
+    @Column(columnDefinition = "numeric", nullable = false)
+    var cost: Float,
+    @Column(nullable = false)
+    var is_cost_per_hour: Boolean
+)
+
+@Entity
+class ServiceHistory(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @ManyToOne(optional = false) val girl: Girls,
+    @ManyToOne(optional = false) val service: PriceList,
+    @ManyToOne(optional = false) val client: Users,
+    @Column(columnDefinition = "timestamp", nullable = false)
+    var start_timestamp: Timestamp,
+    @Column(columnDefinition = "timestamp")
+    var end_timestamp: Timestamp? = null,
+    @Column(columnDefinition = "numeric", nullable = false)
+    var total_cost: Float
+)
+
+@Entity
+class FellholeInfo(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    var address: String,
+    var logo: String,
+    @Column(nullable = false)
+    var name: String
 )
--- src/main/kotlin/com/mpi/kissing_company/repositories/Repositories.kt
+++ src/main/kotlin/com/mpi/kissing_company/repositories/Repositories.kt
@@ -1,12 +1,12 @@
 package com.mpi.kissing_company.repositories
 
-import com.mpi.kissing_company.entities.User
+import com.mpi.kissing_company.entities.Users
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
 
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
-interface PersonRepository : PagingAndSortingRepository<User?, Long?> {
-    fun findByNickname(@Param("nickname") nickname: String?): List<User?>?
+interface PersonRepository : PagingAndSortingRepository<Users?, Long?> {
+    fun findByUsername(@Param("username") username: String?): List<Users?>?
 }
