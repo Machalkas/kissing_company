061a6bd02eae15e2d7d6e090031a0d3064273541
MachalkaMSI <al1999dk@gmail.com>
2022-12-18 01:34:12 +0300

    add /girls/registration

--- src/main/kotlin/com/mpi/kissing_company/controllers/GirlController.kt
+++ src/main/kotlin/com/mpi/kissing_company/controllers/GirlController.kt
@@ -1,26 +1,33 @@
 package com.mpi.kissing_company.controllers
 
 import com.mpi.kissing_company.entities.Girl
+import com.mpi.kissing_company.entities.InviteLinks
 import com.mpi.kissing_company.exceptions.GirlNotFoundException
 import com.mpi.kissing_company.repositories.GirlRepository
+import com.mpi.kissing_company.repositories.InviteLinksRepository
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
+import java.time.LocalDateTime
 import java.util.*
 import java.util.function.Function
 import java.util.function.Supplier
 
 
 @RestController
-internal class GirlController(private val repository: GirlRepository) {
+internal class GirlController(private val repository: GirlRepository,
+                              private val invite_repository: InviteLinksRepository) {
 
     @GetMapping("/girls")
     fun all(): List<Girl?> {
         return repository.findAll()
     }
 
-    @PostMapping("/girls")
-    fun newGirl(@RequestBody newGirl: Girl): Girl{
-        return repository.save(newGirl)
-    }
+//    @PostMapping("/girls")
+//    fun newGirl(@RequestBody newGirl: Girl): Girl{
+//        return repository.save(newGirl)
+//    }
 
     @GetMapping("/girls/{id}")
     fun one(@PathVariable id: String): Girl? {
@@ -28,8 +35,26 @@ internal class GirlController(private val repository: GirlRepository) {
             .orElseThrow(Supplier<RuntimeException> { GirlNotFoundException(id) })
     }
 
+    @PostMapping("/girls/registration/{token}")
+    fun registrateGirl(@RequestBody newGirl: Girl, @PathVariable token: String): Girl {
+        var invite_token: InviteLinks?
+        try {
+            invite_token = invite_repository.findByinviteToken(token)
+        }
+        catch(ex: EmptyResultDataAccessException){
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Invite token not found")
+        }
+        val now = LocalDateTime.now()
+        if (now.isAfter(invite_token.getexpirtyDt())){
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Invite link is expired")
+        }
+        invite_repository.deleteByinviteToken(token)
+        return repository.save(newGirl)
+
+    }
+
     @PutMapping("/girls/{id}")
-    fun replaceGirl(@RequestBody newGirl: Girl, @PathVariable id: String): Optional<Girl>? {
+    fun updateGirl(@RequestBody newGirl: Girl, @PathVariable id: String): Optional<Girl>? {
         return repository.findById(id)
             .map<Girl>(Function { girl: Girl ->
                 girl.setAge(newGirl.getAge())
